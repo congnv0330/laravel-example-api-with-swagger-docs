36066ae37b4a177fe53e1a27b801a211b9bc3846
congnv <congnv3030@gmail.com>
2022-01-26 09:25:22 +0700

    Remove barryvdh/laravel-ide-helper

--- app/Models/Slug.php
+++ app/Models/Slug.php
@@ -5,23 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
-/**
- * App\Models\Slug
- *
- * @property int $id
- * @property string $value
- * @property int $reference_id
- * @property string $reference_type
- * @property-read Model|\Eloquent $sluggable
- * @method static \Illuminate\Database\Eloquent\Builder|Slug newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Slug newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Slug query()
- * @method static \Illuminate\Database\Eloquent\Builder|Slug whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Slug whereReferenceId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Slug whereReferenceType($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Slug whereValue($value)
- * @mixin \Eloquent
- */
 class Slug extends Model
 {
     public $timestamps = false;
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -7,28 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 
-/**
- * App\Models\Tag
- *
- * @property int $id
- * @property string $name
- * @property int $sort_order
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Blog[] $blogs
- * @property-read int|null $blogs_count
- * @property-read \App\Models\Slug|null $slug
- * @method static \Database\Factories\TagFactory factory(...$parameters)
- * @method static \Illuminate\Database\Eloquent\Builder|Tag newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Tag newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Tag query()
- * @method static \Illuminate\Database\Eloquent\Builder|Tag whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Tag whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Tag whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Tag whereSortOrder($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Tag whereUpdatedAt($value)
- * @mixin \Eloquent
- */
 class Tag extends Model
 {
     use HasFactory;
