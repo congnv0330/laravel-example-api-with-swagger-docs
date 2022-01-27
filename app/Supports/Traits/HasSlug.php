78e17c6f6de5b6197e47522b30cfb28eccdc7a5b
congnv <congnv3030@gmail.com>
2022-01-27 14:39:43 +0700

    Add support trait HasSlug

---
+++ app/Supports/Traits/HasSlug.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Supports\Traits;
+
+use App\Models\Slug;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+
+trait HasSlug
+{
+    public function slug(): MorphOne
+    {
+        return $this->morphOne(Slug::class, 'reference');
+    }
+
+    public function getSlugUrlAttribute(): string
+    {
+        return $this->slug?->value;
+    }
+}
